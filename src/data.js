f7892f08db4419c082b92a4b28a30fd5fbe2fd37
clint601 <clintjackson601@gmail.com>
2025-01-14 14:47:26 -0600

    done

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        location: 'Texas',
+        year: 1995,
+        imgUrl: '/image/texas.jpeg',
+        description: 'Went here to hunt hogs like a real redneck.',
+        alt: 'some',
+        isFavorite: false
+    },
+    {
+        location: 'China',
+        year: 2019,
+        imgUrl: '/image/china.jpeg',
+        description: 'A solo vacation because china is number one.',
+        alt:"some",
+        isFavorite: false
+    },
+    {
+        location: 'New York',
+        year: 2020,
+        imgUrl: '/image/york.jpeg',
+        description: 'Went there to get a baccon egg and cheese the ocky way',
+        alt: 'some',
+        isFavorite: false
+    },
+    {
+        location: 'Gotham city',
+        year: 2018,
+        imgUrl: '/image/gotham.jpeg',
+        description: 'Me and the family went to gotham for the first time.while there there was this one night we saws an geown man in a costume on a roof top.',
+        alt: 'some',
+        isFavorite: true
+    },
+    {
+        location: 'Florida',
+        year: 2019,
+        imgUrl: '/image/florida.jpeg',
+        description: 'My Family went down her to vist my dad family.We had a good time down there one of my favorite places to go.',
+        alt: 'some',
+        isFavorite: true
+    },
+    {
+        location: 'Vegas',
+        year: 2022,
+        imgUrl: '/image/vegas.jpeg',
+        description: 'This was my first time in vegas.I had a good time and a bad time here. I won some money then I loss it all.Also I got married',
+        alt: 'some',
+        isFavorite: true
+    }
+]
